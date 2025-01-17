3c0896a63d7c42bc4005f25282cdd7a8b12719c1
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-17 01:34:15 +0100

    add 0, 1, 2 and 3

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastNumber = number % 10
+if lastNumber < 0:
+    lastNumber = -lastNumber
+if lastNumber > 5:
+    print(f"Last digit of {number} is {lastNumber} and is greater than 5")
+elif lastNumber == 0:
+    print(f"Last digit of {number} is {lastNumber} and is 0")
+else:
+    print(f"Last digit of {number} is {lastNumber} and is less than 6 and not 0")
---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for alphabet in range(97, 123):
+    print(chr(alphabet), end="") # chr convert number in ascii char and end="" avoid \n
