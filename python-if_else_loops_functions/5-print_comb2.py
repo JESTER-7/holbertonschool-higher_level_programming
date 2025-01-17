6945238f57e843ed06f525fd80188263b453fec0
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-17 21:03:40 +0100

    fix the 5 and add the 8

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    if i < 10:
-        print("0{}, ".format(i), end="")
+for i in range(100):
+    if i < 99:
+        print("{:02}, ".format(i), end="")  # :02 = 2 digit
     else:
-        print("{}, ".format(i), end="")
-print(" 99")
+        print("{:02}".format(i))
