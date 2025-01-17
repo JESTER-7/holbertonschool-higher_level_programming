c7662696e97e86f33cf5412fd23d7f104893cc5d
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-17 11:13:33 +0100

    test with format

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
-    print("{}".format(chr(alphabet)), end="") # chr convert number in ascii char and end="" avoid \n
+    print("{}".format(chr(alphabet)), end="")
