9fe12bd044236aa4bc97a27a52aa74ac0ebcc494
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-22 09:50:53 +0100

    add the 6 and the 7

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for i, element in enumerate(line):
+            if i < len(line) - 1:
+                print("{:d}".format(element), end=" ")
+            else:
+                print("{:d}".format(element), end="")
+        print()
