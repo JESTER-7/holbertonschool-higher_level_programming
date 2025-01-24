657e44aca683005f2633c0f7e9f5352f59ca9248
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-24 13:57:05 +0100

    add all

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return None
-    for i in range(len(sentence)):
+        return None, 0
+    else:
         return (len(sentence), sentence[0])
