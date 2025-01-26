11538870e0682ac286703dc1e547b63d121b08de
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-26 21:20:57 +0100

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     romanNumber = 0
+    if not roman_string or None:
+        return 0
     for i in range(len(roman_string)):
         if len(roman_string) > i + 1 and roman_string[i] == "I":
             if roman_string[i + 1] == "V" or roman_string[i + 1] == "X":
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     newList = []
     for i in set_1:
-            if i not in set_2:
-                newList.append(i)
+        if i not in set_2:
+            newList.append(i)
     for j in set_2:
-         if j not in set_1:
+        if j not in set_1:
             newList.append(j)
     return newList
