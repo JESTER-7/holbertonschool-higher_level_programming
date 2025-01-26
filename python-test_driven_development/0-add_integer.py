8865a7882497f8b8e93101f338ada03767940317
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-26 22:03:39 +0100

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+Add 2 integer
+add_integer is the function to add integers
+exemple: print(add_integer(4, 6))
+"""
 def add_integer(a, b=98):
+    """
+    Add 2 integers
+    
+    Parameter:
+        a: first int
+        b: second int
+    
+    Return:
+        a + b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
