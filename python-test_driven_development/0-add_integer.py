a2d6987d8219d56ac52f0887642c3e792e3b7037
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-26 22:08:52 +0100

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """
 Add 2 integer
 add_integer is the function to add integers
@@ -7,11 +9,11 @@ exemple: print(add_integer(4, 6))
 def add_integer(a, b=98):
     """
     Add 2 integers
-    
+
     Parameter:
         a: first int
         b: second int
-    
+
     Return:
         a + b
     """
