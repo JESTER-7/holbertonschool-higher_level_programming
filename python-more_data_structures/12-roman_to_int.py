bfb237062572ecc9d870d8b62d574b681a65e223
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-26 21:25:36 +0100

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     romanNumber = 0
-    if not roman_string or None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     for i in range(len(roman_string)):
         if len(roman_string) > i + 1 and roman_string[i] == "I":
