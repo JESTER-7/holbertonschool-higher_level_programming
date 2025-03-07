2593a5ac615129ad34c3dc5c44238768ff737e9b
JESTER-7 <arthur.urbano33@gmail.com>
2025-03-07 23:32:41 +0100

    add

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- database
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.tv_show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY genres.name ASC;
