bd806793014ce129fbeff6866f039c5c82248687
JESTER-7 <arthur.urbano33@gmail.com>
2025-03-07 23:37:25 +0100

    add

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
